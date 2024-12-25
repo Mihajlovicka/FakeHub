f784d5fa14154b77ca82022c1bcb828e54600a5d
Sara Mihajlović <33412004+Mihajlovicka@users.noreply.github.com>
2024-12-25 12:54:45 +0100

    v1.0.0 - User, Organization, Team, and Member Management
    Detailed Changes:
    - Implemented user login, registration, and management.
    - Added CRUD operations for organizations.
    - Added CRUD operations for teams.
    - Integrated organization member management.
    - Set up CI/CD pipelines and Docker configuration.

--- FakeHubClient/src/app/core/services/helper.service.ts
+++ FakeHubClient/src/app/core/services/helper.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
-import moment from 'moment';
+import { Injectable } from "@angular/core";
+import { format, isValid } from "date-fns";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HelperService {
   public formatDate(date: Date): string {
-    return this.isDateValid(date) ? moment(date).local().format('MMMM D, YYYY') : '';
+    return this.isDateValid(date) ? format(date, "MMMM d, yyyy") : "";
   }
 
   public isDateValid(date: Date): boolean {
-    return new Date(date).getTime() > 0;
+    return isValid(date);
   }
 
   public capitalizeFirstLetter(input: string): string {
     if (!input) {
-      return '';
+      return "";
     }
     return input.charAt(0).toUpperCase();
   }
 
-   public readImageBase64(file: File): Promise<string> {
+  public readImageBase64(file: File): Promise<string> {
     return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          resolve(e.target.result);
-        };
-        reader.readAsDataURL(file);
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        resolve(e.target.result);
+      };
+      reader.readAsDataURL(file);
     });
   }
 }
